52a439b2308eef1ad1baf4a890ef50e9c3428340
Rikuto Nishiyama <crispycrack689@gmail.com>
2025-03-08 08:46:03 +0900

    通話通知機能を再現作成

--- SukonbuDiscordBot/Program.cs
+++ SukonbuDiscordBot/Program.cs
@@ -5,6 +5,7 @@ using Discord;
 using Discord.WebSocket;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SukonbuDiscordBot
 {
@@ -13,6 +14,7 @@ namespace SukonbuDiscordBot
         private DiscordSocketClient m_client;
         private ulong m_channelIdVoice;
         private ulong m_channelIdChat;
+        private Dictionary<ulong, DateTime> m_voiceStartTimes = new Dictionary<ulong, DateTime>();
 
         private static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
 
@@ -70,15 +72,65 @@ namespace SukonbuDiscordBot
         {
             if (m_client.GetChannel(m_channelIdVoice) is IMessageChannel channel)
             {
+                var userName = user.Username;
+                var avatarUrl = user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl();
+
                 // ユーザーがボイスチャンネルに入室した
                 if (before.VoiceChannel == null && after.VoiceChannel != null)
                 {
-                    await channel.SendMessageAsync(user.Username + " has joined the voice channel!");
+                    m_voiceStartTimes[user.Id] = DateTime.Now;
+
+                    var channelName = after.VoiceChannel.Name;
+                    var startTime = m_voiceStartTimes[user.Id].ToString("yyyy/MM/dd HH:mm:ss");
+
+                    var embed = new EmbedBuilder()
+                        .WithTitle("通話開始")
+                        .AddField("**`チャンネル`**", channelName, true)
+                        .AddField("**`始めた人`**", userName + "さん", true)
+                        .AddField("**`開始時間`**", startTime, true)
+                        .WithColor(0xff8e8e)
+                        .WithThumbnailUrl(avatarUrl)
+                        .Build();
+
+                    await channel.SendMessageAsync("@everyone", embed: embed);
                 }
-                // ユーザーがボイスチャンネルから退出した
                 else if (before.VoiceChannel != null && after.VoiceChannel == null)
                 {
-                    await channel.SendMessageAsync(user.Username + " has left the voice channel!");
+                    var endTime = DateTime.Now;
+                    var endTimeString = endTime.ToString("yyyy/MM/dd HH:mm:ss");
+
+                    if (m_voiceStartTimes.TryGetValue(user.Id, out var startTime))
+                    {
+                        var channelName = before.VoiceChannel.Name;
+                        var duration = endTime - startTime;
+                        m_voiceStartTimes.Remove(user.Id);
+
+                        // 2桁表示にする
+                        var durationHours = duration.Hours.ToString();
+                        if (durationHours.Length <= 1)
+                        {
+                            durationHours = "0" + durationHours;
+                        }
+                        var durationMinutes = duration.Minutes.ToString();
+                        if (durationMinutes.Length <= 1)
+                        {
+                            durationMinutes = "0" + durationMinutes;
+                        }
+                        var durationSeconds = duration.Seconds.ToString();
+                        if (durationSeconds.Length <= 1)
+                        {
+                            durationSeconds = "0" + durationSeconds;
+                        }
+
+                        var embed = new EmbedBuilder()
+                            .WithTitle("通話終了")
+                            .AddField("**`チャンネル`**", channelName, true)
+                            .AddField("**`通話時間`**", $"{durationHours}:{durationMinutes}:{durationSeconds}", true)
+                            .WithColor(0x8e8eff)
+                            .Build();
+
+                        await channel.SendMessageAsync(embed: embed);
+                    }
                 }
             }
         }
