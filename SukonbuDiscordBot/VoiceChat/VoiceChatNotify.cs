ceb243381d703e1224783bef7e872721835e72e3
Rikuto Nishiyama <crispycrack689@gmail.com>
2025-12-06 13:36:22 +0900

    変数の宣言順をリファクタ

--- SukonbuDiscordBot/VoiceChat/VoiceChatNotify.cs
+++ SukonbuDiscordBot/VoiceChat/VoiceChatNotify.cs
@@ -31,19 +31,14 @@ namespace SukonbuDiscordBot.VoiceChat
 
             if (client.GetChannel(channelIdVoice) is IMessageChannel channel)
             {
-                var userName = user.Username;
-                var avatarUrl = user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl();
-
+                // ユーザーがボイスチャンネルに入室した
                 if (before.VoiceChannel == null && after.VoiceChannel != null)
                 {
-                    // ユーザーがボイスチャンネルに入室した
-
                     // 対象のボイスチャンネルでなければ無視
                     if (!channelIdWatchVoice.Contains(after.VoiceChannel.Id))
                     {
                         return;
                     }
-
                     // すでにユーザーが通話中の場合は無視
                     // すでに通話中の場合は開始時間を更新しない
                     if (after.VoiceChannel.ConnectedUsers.Count > 1)
@@ -54,9 +49,10 @@ namespace SukonbuDiscordBot.VoiceChat
                     // チャンネルIDをキーにして開始時間を保存
                     m_voiceStartTimes[after.VoiceChannel.Id] = DateTime.Now;
 
+                    var userName = user.Username;
+                    var avatarUrl = user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl();
                     var channelName = after.VoiceChannel.Name;
                     var startTime = m_voiceStartTimes[after.VoiceChannel.Id].ToString("yyyy/MM/dd HH:mm:ss");
-
                     var embed = new EmbedBuilder()
                         .WithTitle("通話開始")
                         .AddField("**`チャンネル`**", channelName, true)
@@ -68,30 +64,27 @@ namespace SukonbuDiscordBot.VoiceChat
 
                     await channel.SendMessageAsync("@everyone", embed: embed);
                 }
+                // ユーザーがボイスチャンネルから退出した
                 else if (before.VoiceChannel != null && after.VoiceChannel == null)
                 {
-                    // ユーザーがボイスチャンネルから退出した
-
-                    var endTime = DateTime.Now;
-
                     // 通話チャンネルにまだ人がいる場合は通知しない
                     if (before.VoiceChannel.ConnectedUsers.Count > 0)
                     {
                         return;
                     }
-
+                    // 通話開始時間が取得できなかった
                     if (!m_voiceStartTimes.TryGetValue(before.VoiceChannel.Id, out var startTime))
                     {
-                        // 通話開始時間が取得できなかった
                         await Log.Trace(LogSeverity.Info, $"Coudn't get chat start time: {before.VoiceChannel.Name}");
                         return;
                     }
 
-                    var channelName = before.VoiceChannel.Name;
-                    var duration = endTime - startTime;
+                    // 開始時間の情報を削除
                     m_voiceStartTimes.Remove(before.VoiceChannel.Id);
 
                     // 2桁表示にする
+                    var endTime = DateTime.Now;
+                    var duration = endTime - startTime;
                     var durationHours = duration.Hours.ToString();
                     if (durationHours.Length <= 1)
                     {
@@ -108,6 +101,7 @@ namespace SukonbuDiscordBot.VoiceChat
                         durationSeconds = "0" + durationSeconds;
                     }
 
+                    var channelName = before.VoiceChannel.Name;
                     var embed = new EmbedBuilder()
                         .WithTitle("通話終了")
                         .AddField("**`チャンネル`**", channelName, true)
